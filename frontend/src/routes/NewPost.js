815cbe342e718b4eef108ca03054654a1f4e8425
joaogsleite <joaogsleite@gmail.com>
2018-09-16 14:44:47 +0100

    static new post page

--- frontend/src/routes/NewPost.js
+++ frontend/src/routes/NewPost.js
@@ -1,5 +1,27 @@
-import React from 'react'
+import React,{Component} from 'react'
+import {TextField, SelectField, ActionButton} from '../components/forms'
 
-export default function NewPost(props){
-	return <div>NewPost</div>
+export default class NewPost extends Component {
+	constructor(props){
+		super(props)
+		this.state = {}
+	}
+	onChange = (name,value)=>{
+		this.setState({[name]:value})
+	}
+	save = ()=>{
+		console.log('save')
+	}
+	render(){
+		const {title,body,author,category} = this.state
+		const categories = ['a','b','c']
+		return <div>
+			<h1>New post</h1>
+			<TextField name="title" value={title} onChange={this.onChange} />
+			<TextField name="body" value={body} onChange={this.onChange} />
+			<TextField name="author" value={author} onChange={this.onChange} />
+			<SelectField name="category" value={category} options={categories} onChange={this.onChange} />
+			<ActionButton color="green" name="Save" action={this.save} />
+		</div>
+	}
 }
