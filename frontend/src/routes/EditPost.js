17dd3f147584a93660567d7c915da678a61c2d99
joaogsleite <joaogsleite@gmail.com>
2018-09-16 14:58:39 +0100

    edit post page

--- frontend/src/routes/EditPost.js
+++ frontend/src/routes/EditPost.js
@@ -1,5 +1,26 @@
-import React from 'react'
+import React,{Component} from 'react'
 
-export default function EditPost({match}){
-	return <div>EditPost: {match.params.post}</div>
+import PageTitle from '../components/layout/PageTitle'
+import {TextField,ActionButton} from '../components/forms'
+
+export default class EditPost extends Component {
+	constructor(props){
+		super(props)
+		this.state = {}
+	}
+	onChange = (name,value)=>{
+		this.setState({[name]:value})
+	}
+	save = ()=>{
+
+	}
+	render(){
+		const {title, body} = {title: "Example", body: "Example"}
+		return <div>
+			<PageTitle title="Edit post" />
+			<TextField name="title" value={title} onChange={this.onChange} />
+			<TextField name="body" value={body} onChange={this.onChange} />
+			<ActionButton color="green" name="Save" action={this.save} />
+		</div>
+	}
 }
