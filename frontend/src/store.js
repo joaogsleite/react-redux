e0bdd2195514cf8b76f0938f3cf0ca95d8f66508
joaogsleite <joaogsleite@gmail.com>
2018-09-16 16:07:41 +0100

    created redux store

---
+++ frontend/src/store.js
@@ -0,0 +1,16 @@
+import {applyMiddleware, createStore} from 'redux'
+
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+import promise from 'redux-promise-middleware'
+
+import reducer from './reducers'
+
+const middleware = applyMiddleware(promise(),thunk,logger)
+const store = createStore(reducer, middleware)
+
+// only for debuguing
+window.store = store.getState
+
+export default store
+
